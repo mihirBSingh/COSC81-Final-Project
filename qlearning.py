432d0ef2f47eb780d9bb9c24be44a34aa37ccaa0
Megan Liu <mgnliu20@gmail.com>
2025-06-01 11:50:01 -0400

    created qtable correctly based on gridmap

--- qlearning.py
+++ qlearning.py
@@ -4,14 +4,18 @@ import rclpy
 from mapping import GridMapper
 
 class QLearningAgent:
-    def __init__(self, discount_rate=0.9, learning_rate=0.1, exploration_rate=0.1, initial_size=1000):
-        self.q_table = np.zeros((initial_size, initial_size, 4))  # x,y,q-value 
+    def __init__(self, discount_rate=0.9, learning_rate=0.1, exploration_rate=0.1, initial_size=1000, res=0.05):
+        grid_size = int(initial_size * res)
+        self.q_table = np.zeros((grid_size, grid_size, 4))  # x,y,q-value -- x,y in m
+        self.origin_x = round(initial_size / 2.0 * res)
+        self.origin_y = round(initial_size / 2.0 * res)
 
         self.learning_rate = learning_rate
         self.discount_factor = discount_rate
         self.exploration_rate = exploration_rate 
+        self.res = res 
 
-        print(f"Q-table initialized with shape: {self.q_table.shape}\n")
+        print(f"Q-table initialized with shape: {self.q_table.shape} and origin: {self.origin_x}, {self.origin_y}\n")
 
     def choose_action(self, state):
         if random.uniform(0, 1) < self.exploration_rate:
@@ -42,13 +46,14 @@ class QLearningAgent:
         print(f"Training for {num_episodes} episodes...")
         for _ in range(num_episodes):
             state = grid.reset()
+            state_offset = (state[0] + self.origin_x, state[1] + self.origin_y)  # offset by origin 
             done = False
             print(f" --- State (px): {state} --- ")
             while not done:
                 action = self.choose_action(state)
                 next_state, reward, done = grid.step(action, reward_type)
-                next_state = (next_state[0], next_state[1])
-                self.update_q_value(state, action, reward, next_state)
+                next_state_offset = (next_state[0] + self.origin_x, next_state[1] + self.origin_y)  # offset by origin 
+                self.update_q_value(state_offset, action, reward, next_state_offset)
                 state = next_state
                 print(f" --- State (px): {state} --- ")
             self.expand_qtable(grid.size)
@@ -57,20 +62,21 @@ def main(args=None):
     rclpy.init(args=args)
     
     initial_size = 1000 
+    res = 0.05
 
     # odom px
     startx = 0 
     starty = 0 
     goal = (initial_size-1, initial_size-1)
 
-    gm_node = GridMapper(goal=goal, pos_x=startx, pos_y=starty, initial_size=initial_size)
+    gm_node = GridMapper(goal=goal, pos_x=startx, pos_y=starty, initial_size=initial_size, res=res)
 
     # hyperparameters TODO: tune
     # discount_factor = 0.4
     # learning_rate = 0.1 
     # exploration_rate = 0.1
 
-    q = QLearningAgent(initial_size=initial_size)
+    q = QLearningAgent(initial_size=initial_size, res=res)
 
     q.train(100, gm_node)
 
